f2ebf9d87012bc13df202259f6cf7f73e4ac7b0c
ghuy311 <hogiahy311@gmail.com>
2025-06-04 21:35:29 +0700

    39

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 app.get("/", (req,res) => {
-    res.send("Hello World") 
+    res.send("Hello World update") 
 }) 
 
 app.listen(PORT, () => {
