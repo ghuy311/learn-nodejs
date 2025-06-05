0ab03ec6c8629c51bdfd07f7f4089696e89303ff
ghuy311 <hogiahy311@gmail.com>
2025-06-05 22:26:18 +0700

    49

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+const getHomePage = (req : Request, res : Response) => {
+    return res.render("home")
+    
+}
+
+const getCreateUserPage = (req : Request, res : Response) => {
+    return res.render("create-user")
+    
+}
+
+export { getHomePage,getCreateUserPage };
