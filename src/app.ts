0c5fc7de085d2c5c15cfb90bf0673f356742fa71
ghuy311 <hogiahy311@gmail.com>
2025-06-05 21:21:58 +0700

    45

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from "express";
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req,res) => {
-    res.send("Hello World nodemon") 
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.get("/", (req, res) => {
+    res.render("home.ejs")
 }) 
 
 app.listen(PORT, () => {
