67650eb04caa255cdae33750add7e4c33d27e378
ghuy311 <hogiahy311@gmail.com>
2025-06-11 21:02:57 +0700

    52

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getAllUser, handleCreateUser, handleDeleteUser } from "services/user.service";
+import { getAllUser, getUserByID, handleCreateUser, handleDeleteUser } from "services/user.service";
 
 const getHomePage = async(req: Request, res: Response) => {
     // Get users
@@ -27,4 +27,15 @@ const postDeleteUser = async (req: Request, res: Response) => {
     await handleDeleteUser(id);
     return res.redirect("/");
 }
-export { getHomePage,getCreateUserPage, postCreateUser, postDeleteUser };
+
+const getViewUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // get user by id
+    const user = await getUserByID(id); 
+    return res.render("view-user.ejs", {
+        id: id,
+        user : user
+    })
+
+}
+export { getHomePage,getCreateUserPage, postCreateUser, postDeleteUser, getViewUser };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -46,5 +46,18 @@ const getAllUser = async () => {
   }
 }
 
+const getUserByID = async(id: string) => {
+  try {
+    const connection = await getConnection();
+  const sql = 'SELECT *  FROM `users` WHERE `id` = ?';
+  const values = [id];
 
-export{handleCreateUser, getAllUser,handleDeleteUser}
+  const [result, fields] = await connection.execute(sql, values);
+    return result[0];
+} catch (err) {
+    console.log(err);
+    return [];
+}
+} 
+
+export{handleCreateUser, getAllUser,handleDeleteUser, getUserByID}
