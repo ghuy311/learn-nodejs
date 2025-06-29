1f77f7e18cbc32256f591953b1b05a4bae5cd35c
ghuy311 <hogiahy311@gmail.com>
2025-06-29 21:53:50 +0700

    middleware mutler

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,8 +1,7 @@
 import express, {Express} from 'express';
 import { getCreateUserPage, getHomePage, postCreateUser,postDeleteUser,getViewUser, postUpdateUser } from 'controllers/user.controller';
 import { getAdminUserPage, getDashboardPage, getAdminProductPage,getAdminOrderPage } from 'controllers/admin/dashboard.controllers';
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+import fileUploadMiddleware from 'src/middleware/multer';
 
 const router = express.Router();
 
@@ -17,13 +16,9 @@ const webRoutes = (app : Express) => {
     router.get("/admin", getDashboardPage);
     router.get("/admin/user", getAdminUserPage);
     router.get("/admin/create-user", getCreateUserPage);
-    // router.post("/handle-create-user", postCreateUser);
-    router.post("/handle-create-user", upload.single('avatar'), (req, res) => {
-        res.send("ok")
-    });
+    router.post("/handle-create-user",fileUploadMiddleware("avatar"), postCreateUser);
 
-    
-    
+
     router.get("/admin/product", getAdminProductPage);
     router.get("/admin/order", getAdminOrderPage);
     app.use("/", router);
