5f920ada2139f2a396e8bf79c91b7afe3e62abed
ghuy311 <hogiahy311@gmail.com>
2025-06-30 11:37:39 +0700

    hash user password

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,21 +1,26 @@
+import { hashPassword } from "services/user.service";
 import { prisma } from "./client"
+import { ACCOUNT_TYPE } from "config/constant";
 
 const initDatabase = async () => {
     const countUser = await prisma.user.count();
     const countRole = await prisma.role.count();
     if (countUser == 0) {
+        const defaultPassword = await hashPassword("123456");
         await prisma.user.createMany({
             data: [
                 {
-                    username: "huy@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM",
+                    fullName : "Hỏi dân IT",
+                    username: "hoidanit@gmail.com",
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM,
                 }
                 ,
                 {
+                    fullName : "Admin",
                     username: "admin@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM",
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM,
                 }
             ]
         })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,21 @@
 import { prisma } from "config/client";
 import { ACCOUNT_TYPE } from "config/constant";
+import bcrypt from'bcrypt';
+const saltRounds = 10;
 
+const hashPassword = async(plainText : string) => {
+  return await bcrypt.hash(plainText, saltRounds);
+}
 const handleCreateUser = async(fullName: string, email: string, address: string, phone : string, avatar : string) => {
-    //insert into database
-    //return result
-
-   const newUser = await prisma.user.create({
+  //insert into database
+  //return result
+  const defaultPassword = await hashPassword("123456");
+  const newUser = await prisma.user.create({
     data: {
       fullName: fullName,
       username: email,
       address: address,
-      password: "123456",
+      password: defaultPassword,
       accountType: ACCOUNT_TYPE.SYSTEM,
       avatar: avatar,
       phone : phone
@@ -51,4 +56,4 @@ const updateUserByID = async (id: string, email: string, address: string, fullNa
   return updateUser;
 } 
 
-export{handleCreateUser, getAllUser,handleDeleteUser, getUserByID,updateUserByID, getAllRoles}
+export{handleCreateUser, getAllUser,handleDeleteUser, getUserByID,updateUserByID, getAllRoles, hashPassword}
